b4c755fdce8d4e5b1068fa9742b196983bbf2225
Bheem mishra <mr.siddhivinayakmishra@gmail.com>
2026-07-20 19:17:50 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -6,22 +6,32 @@ import {
   updateProfile
 } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js";
 
-// Signup
-const signupBtn = document.getElementById("signupBtn");
-
 if (signupBtn) {
-  signupBtn.addEventListener("click", () => {
+  signupBtn.addEventListener("click", async () => {
+
+    const username = document.getElementById("username").value.trim();
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
 
-    createUserWithEmailAndPassword(auth, email, password)
-      .then(() => {
-        alert("Account Created Successfully!");
-        window.location.href = "login.html";
-      })
-      .catch((error) => {
-        alert(error.message);
+    if (!username) {
+      alert("Please enter a username.");
+      return;
+    }
+
+    try {
+      const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+
+      await updateProfile(userCredential.user, {
+        displayName: username
       });
+
+      alert("Account Created Successfully!");
+      window.location.href = "login.html";
+
+    } catch (error) {
+      alert(error.message);
+    }
+
   });
 }
 
