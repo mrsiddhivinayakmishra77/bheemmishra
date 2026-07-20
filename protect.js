df992c6bacaf516ea1e6032c0f7268d4a1f7f6d3
Bheem mishra <mr.siddhivinayakmishra@gmail.com>
2026-07-20 11:12:50 +0530

    Create protect.js

---
+++ protect.js
@@ -0,0 +1,10 @@
+import { auth } from "./firebase-config.js";
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js";
+
+onAuthStateChanged(auth, (user) => {
+
+  if (!user) {
+    window.location.href = "login.html";
+  }
+
+});
