5642e61494ae58edcc5a78fa9b927e91f7bc2fde
Bheem mishra <mr.siddhivinayakmishra@gmail.com>
2026-07-20 21:28:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -109,26 +109,15 @@ async function sendMessage() {
 
   try {
 
-  const response = await fetch(
-    "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=AIzaSyDfUKnFQFKQulGjYhwc2QIu2QobEtb9lF4",
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        contents: [
-          {
-            parts: [
-              {
-                text: msg
-              }
-            ]
-          }
-        ]
-      })
-    }
-  );
+  const response = await fetch("/api/gemini", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json"
+  },
+  body: JSON.stringify({
+    message: msg
+  })
+});
 
 const data = await response.json();
 console.log(data);
