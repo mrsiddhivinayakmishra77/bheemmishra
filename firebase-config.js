1716071f6d71f4b31fe066dbe0a739223002f7cc
Bheem mishra <mr.siddhivinayakmishra@gmail.com>
2026-07-20 10:45:19 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,10 @@
-
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDjuKt7KWlvN1feMF0LYzb8j3lYhUzC5bU",
+  authDomain: "bheem-ai-for-upsc.firebaseapp.com",
+  projectId: "bheem-ai-for-upsc",
+  storageBucket: "bheem-ai-for-upsc.firebasestorage.app",
+  messagingSenderId: "1021922130060",
+  appId: "1:1021922130060:web:a3933105cdae8e8d16a3a9",
+  measurementId: "G-09MZPBZY1J"
+};
