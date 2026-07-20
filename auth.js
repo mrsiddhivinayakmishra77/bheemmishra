659fa7fe469f68863568dabdc5127f56776a8960
Bheem mishra <mr.siddhivinayakmishra@gmail.com>
2026-07-20 11:03:39 +0530

    Create auth.js

---
+++ auth.js
@@ -0,0 +1,44 @@
+import { auth } from "./firebase-config.js";
+
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js";
+
+// Signup
+const signupBtn = document.getElementById("signupBtn");
+
+if (signupBtn) {
+  signupBtn.addEventListener("click", () => {
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    createUserWithEmailAndPassword(auth, email, password)
+      .then(() => {
+        alert("Account Created Successfully!");
+        window.location.href = "login.html";
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
+  });
+}
+
+// Login
+const loginBtn = document.getElementById("loginBtn");
+
+if (loginBtn) {
+  loginBtn.addEventListener("click", () => {
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    signInWithEmailAndPassword(auth, email, password)
+      .then(() => {
+        alert("Login Successful!");
+        window.location.href = "index.html";
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
+  });
+}
